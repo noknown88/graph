dcd1464bdff5e7074a4d4646000facb452167fa6
huangxin <huangxin@geotmt.com>
2018-07-01 06:38:36 +0800

    init

---
+++ runserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf ranks.csv
+
+$SPARK_HOME/bin/spark-submit \
+  --class "FinGraphServer" \
+  --master "local[*]" \
+  --packages "graphframes:graphframes:0.5.0-spark2.1-s_2.11" \
+  ./build/libs/fingraph-0.0.1-all.jar 1
