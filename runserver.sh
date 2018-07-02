3976936ce3e98d370916fb2edd35241b0d5cf268
huangxin <huangxin@geotmt.com>
2018-07-02 17:16:19 +0800

    debug

--- runserver.sh
+++ runserver.sh
@@ -3,8 +3,7 @@
 rm -rf ranks.csv
 
 $SPARK_HOME/bin/spark-submit \
-  --class "FinGraphServer" \
+  --class "FinGraphServerDebug" \
   --master "local[*]" \
-  --executor-memory 8G \
   --packages "graphframes:graphframes:0.5.0-spark2.1-s_2.11" \
   ./build/libs/fingraph-0.0.1-all.jar 1
