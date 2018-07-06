2e37a611056296e49e035299aff4ca9da7a402cb
huangxin <huangxin@geotmt.com>
2018-07-06 17:42:25 +0800

    spark1.6 version

--- runserver.sh
+++ runserver.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-rm -rf ranks.csv
 
 $SPARK_HOME/bin/spark-submit \
-  --class "FinGraphServerDebug" \
+  --class "FinGraphServer" \
   --master "yarn" \
-  --executor-memory 8G \
-  --driver-memory 8G \
-  --packages "graphframes:graphframes:0.5.0-spark2.1-s_2.11" \
-  ./build/libs/fingraph-0.0.1-all.jar 1
+  --packages "graphframes:graphframes:0.5.0-spark1.6-s_2.11,com.databricks:spark-csv_2.10:1.5.0" \
+  ./build/libs/fingraph-0.0.1-all.jar 10
