66a22b0292d791d236e13f4d0efff5b984324be3
huangxin <huangxin@geotmt.com>
2018-07-06 17:48:49 +0800

    1

--- runserver.sh
+++ runserver.sh
@@ -4,5 +4,7 @@
 $SPARK_HOME/bin/spark-submit \
   --class "FinGraphServer" \
   --master "yarn" \
+  --executor-memory 8G \
+  --driver-memory 8G \
   --packages "graphframes:graphframes:0.5.0-spark1.6-s_2.11,com.databricks:spark-csv_2.10:1.5.0" \
   ./build/libs/fingraph-0.0.1-all.jar 10
